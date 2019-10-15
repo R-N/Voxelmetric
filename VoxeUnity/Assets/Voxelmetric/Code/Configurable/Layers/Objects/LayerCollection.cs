3d23d9c5e654cb94b41dce9a493b5741f5dd4007
Hertzole <hampusringstrom@gmail.com>
2019-10-15 22:37:24 +0200

    Forgot some files.

---
+++ VoxeUnity/Assets/Voxelmetric/Code/Configurable/Layers/Objects/LayerCollection.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Layer Collection", menuName = "Voxelmetric/Layers/Layer Collection", order = -100)]
+public class LayerCollection : ScriptableObject
+{
+    [SerializeField]
+    private LayerConfigObject[] layers = new LayerConfigObject[0];
+
+    public LayerConfigObject[] Layers { get { return layers; } }
+}
---
+++ VoxeUnity/Assets/Voxelmetric/Code/Configurable/Layers/Objects/LayerConfigObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public abstract class LayerConfigObject : ScriptableObject
+{
+    [SerializeField]
+    private string layerName = "New Layer";
+    [SerializeField]
+    private int index = 0;
+    [SerializeField]
+    private string blockName = "block";
+
+    public string LayerName { get { return layerName; } }
+    public string BlockName { get { return blockName; } }
+
+    public int Index { get { return index; } }
+
+    public abstract TerrainLayer GetLayer();
+
+    public virtual bool IsStructure()
+    {
+        return false;
+    }
+}
